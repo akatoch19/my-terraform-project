bd3d94181d7e72bf144a5b44bcd25f5e1df1f521
arvindkatoch <akatoch19@gmail.com>
2026-04-29 05:16:27 +0530

    modified main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-/*resource "aws_instance" "myec2" {
+resource "aws_instance" "myec2" {
     for_each = {
         Instance-1    = "t2.micro"
         Instance-2 = "t3.micro"
@@ -13,7 +13,7 @@
     }
 }
 
-resource "aws_iam_user" "team" {
+/*resource "aws_iam_user" "team" {
   for_each = toset(["Hilary", "Made", "Cynthia", "Celestine","Arvind"])
   name     = each.value
 }
